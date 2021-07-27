833cfefd546ba7f5dcf06c67aa2c3da2a0778f68
Sam Wells <swells6@gmail.com>
2021-07-27 16:17:06 -0400

    finishing touches, added aria features and favicon

--- script.js
+++ script.js
@@ -7,13 +7,19 @@ $(function(){
     // Create an event listener on the menu titles that displays or hides the menu items on click. 
     $('li.menu-title').on('click',function(){
         // check whether the parent list of the clicked element currently has the "selected" class.
-        // if it does, remove the selected class from the clicked list.
         if($(this).parent().hasClass('selected')){
+              // if it does, remove the selected class from the clicked list.
             $(this).parent().removeClass('selected');
+            // for accessibility: change the aria-expanded attribute on the clicked button to false.
+            $(this).children('button').attr('aria-expanded','false')
         } else{
             // if it doesn't, remove the 'selected' class from whatever list currently has it, and add it to the clicked list.
             $('.selected').removeClass('selected');
             $(this).parent().addClass('selected');
+            // for accessibility: change the aria - expanded attribute on the newly closed menu's button to false.
+            $('.selected').children('button').attr('aria-expanded', 'false')
+            // for accessibility: change the aria - expanded attribute on the newly opened menu's button to true.
+            $(this).children('button').attr('aria-expanded', 'true')
         }
         // then, hide any menu items that are currently shown.
         $('.menu-item').slideUp();
@@ -25,4 +31,3 @@ $(function(){
         $('.selected').find('.expand-cue').text('—')
     })
 })
-
